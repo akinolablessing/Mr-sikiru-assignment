c473751c06403149c98d2452f04dc3a610b1b990
akinolablessing <akinolablessing1890@gmail.com>
2025-02-24 09:41:57 +0100

    assignment

---
+++ html-css-course/buttomTasx.php
@@ -0,0 +1,12 @@
+<style>
+.Request-button{
+    color: white;
+    background-size: 1px;
+    background-color: black;
+    border-top-right-radius: 2px;
+    
+}
+</style>
+<button class="Request-button">
+    Request now
+    </button>
